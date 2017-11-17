222bd52dce74225431caf333d1c5ffd1cc0f4ba8
Eavan Pattie <eavan@lyse.net>
2017-11-17 21:50:50 +0000

    added added api view and added referral as an app

--- referral/views.py
+++ referral/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
+
+def add_referral(request):
+    data = json.loads(request.body.decode("ascii"))
+    return HttpResponse("Hello World!" + json.dumps(data))
