3a1a881ea850b2672115b1f5d92689aaf04f4e05
Michal Kozlowski <Michal@Michals-MacBook-Air.local>
2017-11-18 00:18:43 +0000

    stuff

--- referral/models.py
+++ referral/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from datetime import datetime
+
 # Create your models here.
 class Referral(models.Model):
     sender = models.CharField(max_length=50)
@@ -11,6 +13,7 @@ class Referral(models.Model):
     urgency = models.BooleanField()
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return sender + ' referred ' + name + ' for ' + category + ' issues.'
