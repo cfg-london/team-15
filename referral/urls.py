0318411e9b95b5d672d6b389a61ee0f21f257532
Alex Motoc <alexmotoc97@gmail.com>
2017-11-18 01:13:38 +0000

    Added login template

--- referral/urls.py
+++ referral/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
         url(r'^add$', views.add_referral, name="add_referral"),
+        url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
--- referral/views.py
+++ referral/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import redirect, render
 import json
 
 from .models import Referral, Category
@@ -29,3 +30,23 @@ def add_referral(request):
     category.referrals.add(referral)
 
     return HttpResponse(json.dumps(data))
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            user_object = form.cleaned_data
+            username = user_object['username']
+            password = user_object['password']
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect(main)
+            else:
+                raise forms.ValidationError(_('Username or password are incorrect!'))
+    else:
+        form = LoginForm()
+
+    return render(request, 'tournaments/index.html', {'form': form})
